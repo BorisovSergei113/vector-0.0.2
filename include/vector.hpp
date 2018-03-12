9a72fcc2cc70db0c5000d3775cc3142c4110a0d3
BorisovSergei113 <borisov_sergei113@mail.ru>
2018-03-12 21:24:53 +0300

    vector_example

--- include/vector.hpp
+++ include/vector.hpp
@@ -3,25 +3,25 @@
 class vector_t
 {
 private:
-	int * elements_;
-	std::size_t size_;
-	std::size_t capacity_;
+    int * elements_;//указатель на массив
+    std::size_t size_;// кол-во элементов находящийся в массиве
+    std::size_t capacity_; //кол-во эл для выделения памяти
 public:
-	vector_t();
-	vector_t(vector_t const & other);
-	vector_t & operator =(vector_t const & other);
-	~vector_t();
-
-	std::size_t size() const;
-	std::size_t capacity() const;
-
-	void push_back(int value);
-	void pop_back();
-
-	int & operator [](std::size_t index);
-	int operator [](std::size_t index) const;
-
-	bool operator ==(vector_t const & other) const;
+    vector_t();
+    vector_t(vector_t const & other);
+    vector_t & operator =(vector_t const & other);
+    ~vector_t();
+    
+    std::size_t size() const;
+    std::size_t capacity() const;
+    
+    void push_back(int value);
+    void pop_back();
+    
+    int & operator [](std::size_t index);
+    int operator [](std::size_t index) const;
+    bool operator ==(vector_t const & other) const;
+  friend  bool operator !=(vector_t const & lhs, vector_t const & rhs);
+    
 };
-
-bool operator !=(vector_t const & lhs, vector_t const & rhs);
+ bool operator !=(vector_t const & lhs, vector_t const & rhs);
