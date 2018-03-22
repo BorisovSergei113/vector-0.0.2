dfa40306aabb4f5270de8c7e037cc21babbfe2cf
BorisovSergei113 <borisov_sergei113@mail.ru>
2018-03-23 00:19:07 +0300

    vector0.02

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <algorithm>
+#include <cassert>
 
+template <typename  Type>
+class vector_t;
+
+template <class  Type>
+bool operator !=(vector_t<Type> const & lhs, vector_t<Type> const & rhs);
+
+template <typename  Type>
 class vector_t
 {
 private:
-    int * elements_;//указатель на массив
+    Type * elements_;//указатель на массив
     std::size_t size_;// кол-во элементов находящийся в массиве
     std::size_t capacity_; //кол-во эл для выделения памяти
 public:
@@ -15,13 +24,170 @@ public:
     std::size_t size() const;
     std::size_t capacity() const;
     
-    void push_back(int value);
+    void push_back(Type value);
     void pop_back();
+    Type at(std::size_t index) const ;
+    Type& at(std::size_t index);
     
-    int & operator [](std::size_t index);
-    int operator [](std::size_t index) const;
+    Type & operator [](std::size_t index);
+    Type operator [](std::size_t index) const;
     bool operator ==(vector_t const & other) const;
-  friend  bool operator !=(vector_t const & lhs, vector_t const & rhs);
-    
+    //Специализация шаблона,поэтому выносим объявление перед шаблоном класса
+    friend  bool operator  != <Type>(vector_t<Type> const & lhs, vector_t<Type> const & rhs);
+    //При после инстанциации будет нешаблонная friend декларация и останется одна шаблонная.Из-за этого компилятор не находит реализацию friend декларации
 };
- bool operator !=(vector_t const & lhs, vector_t const & rhs);
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename  Type>
+vector_t<Type>::vector_t()
+{
+    elements_ = nullptr;
+    size_ = 0;
+    capacity_ = 0;
+}
+
+template <typename  Type>
+vector_t<Type>::vector_t(vector_t const & other)
+{
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+    elements_ = new Type [capacity_];
+    for (std::size_t i = 0; i<capacity_; i++) {
+        elements_[i] = other.elements_[i];
+    }
+}
+
+template <typename  Type>
+vector_t<Type> & vector_t<Type>::operator =(vector_t const & other)
+{
+    if (&other == this) return *this;
+    if (elements_!=nullptr) {
+        delete [] elements_;
+    }
+    
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+    elements_ = new Type [capacity_];
+    for (std::size_t i = 0; i<capacity_; i++) {
+        elements_[i] = other.elements_[i];
+    }
+    return *this;
+}
+
+template <typename  Type>
+bool vector_t<Type>::operator ==(vector_t const & other) const
+{
+    if(size_==other.size_){
+        for (std::size_t i = 0; i<capacity_; i++) {
+            if (elements_[i] != other.elements_[i]) return false ;
+        }
+        return true;
+    }
+    else return false;
+}
+
+template <typename  Type>
+vector_t<Type>::~vector_t()
+{
+    if(elements_!=nullptr){
+        delete [] elements_;
+    }
+}
+
+template <typename  Type>
+std::size_t vector_t<Type>::size() const
+{
+    return size_;
+}
+
+template <typename  Type>
+std::size_t vector_t<Type>::capacity() const
+{
+    return capacity_;
+}
+
+template <typename  Type>
+void vector_t<Type>::push_back( Type value )
+{
+    size_++;
+    if (size_>capacity_) {
+        if(!capacity_){
+            capacity_=1;
+        }
+        else{
+            capacity_*=2;
+        }
+        
+        Type *elements = new Type [capacity_];
+        for (std::size_t i = 0; i<size_-1; i++) {
+            elements[i]=elements_[i];
+        }
+        elements[size_-1]=value;
+        if (elements_!=nullptr) delete [] elements_;
+        elements_=elements;
+    }
+    else elements_[size_-1]=value;
+}
+
+template <typename  Type>
+void vector_t<Type>::pop_back()
+{
+    if(!size_) return;
+    size_--;
+    if (size_<=(capacity_/4)) {
+        capacity_=capacity_/2;
+        Type *elements =new Type [capacity_];
+        for (std::size_t i = 0; i<size_; i++) {
+            elements[i]=elements_[i];
+        }
+        if (elements_!=nullptr) delete [] elements_;
+        elements_=elements;
+    }
+}
+
+template <typename  Type>
+Type & vector_t<Type>::operator [](std::size_t index)
+{
+    return elements_[index];
+}
+
+template <typename  Type>
+Type vector_t<Type>::operator [](std::size_t index) const
+{
+    return elements_[index];
+}
+
+template <class  Type>
+bool operator !=(vector_t<Type> const & lhs, vector_t<Type> const & rhs)
+{
+    if(rhs.size()!=lhs.size()){
+        return true;
+    }
+    for (std::size_t i=0; i<rhs.size(); i++) {
+        if (lhs.elements_[i]!=rhs.elements_[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template<typename Type>
+Type  vector_t<Type>::at(std::size_t index) const
+{
+    if(index>=size_){
+        throw "Out of range";
+    }
+    return elements_[index];
+}
+
+template<typename Type>
+Type& vector_t<Type>::at(std::size_t index)
+{
+    if(index>=size_){
+        throw "Out of range";
+    }
+    return elements_[index];
+}
+
--- tests/vector.cpp
+++ tests/vector.cpp
@@ -5,115 +5,131 @@
 
 TEST_CASE("creating vector")
 {
-	vector_t vector;
-	REQUIRE( vector.size() == 0 );
-	REQUIRE( vector.capacity() == 0 );
+    vector_t<float> vector;
+    REQUIRE( vector.size() == 0 );
+    REQUIRE( vector.capacity() == 0 );
 }
 
 TEST_CASE("copying vector")
 {
-	vector_t vector;
-	vector.push_back(1);
-
-	vector_t copy(vector);
-	REQUIRE( copy == vector );
+    vector_t<int> vector;
+    vector.push_back(1);
+    
+    vector_t<int> copy(vector);
+    REQUIRE( copy == vector );
 }
 
 TEST_CASE("assigning vector")
 {
-	vector_t vector1;
-	vector_t vector2;
-
-	vector1.push_back(1);
-	vector2.push_back(2);
-
-	vector1 = vector2;
-	REQUIRE( vector1 == vector2 );
+    vector_t<float> vector1;
+    vector_t<float> vector2;
+    
+    vector1.push_back(1);
+    vector2.push_back(2);
+    
+    vector1 = vector2;
+    REQUIRE( vector1 == vector2 );
 }
 
 TEST_CASE("equaling vector")
 {
-	vector_t vector1;
-	vector_t vector2;
-
-	vector1.push_back(1);
-	vector2.push_back(1);
-
-	REQUIRE( vector1 == vector2 );
-
-	vector1.push_back(2);
-	REQUIRE( vector1 != vector2 );
+    vector_t<float> vector1;
+    vector_t<float> vector2;
+    
+    vector1.push_back(1);
+    vector2.push_back(1);
+    
+    REQUIRE( vector1 == vector2 );
+    
+    vector1.push_back(2);
+    REQUIRE( vector1 != vector2 );
 }
 
 TEST_CASE("indexing vector")
 {
-	vector_t vector;
-
-	vector.push_back(1);
-
-	REQUIRE( vector[0] == 1 );
-
-	vector_t const copy(vector);
-	REQUIRE( copy[0] == 1 );
+    vector_t<float> vector;
+    
+    vector.push_back(1);
+    
+    REQUIRE( vector[0] == 1 );
+    
+    vector_t<float> const copy(vector);
+    REQUIRE( copy[0] == 1 );
+    
+    vector.push_back(2.3);
+    REQUIRE( vector[1] == 2.3f );
+    
 }
 
 TEST_CASE("pushing elements")
 {
-	vector_t vector;
-
-	vector.push_back(1);
-	REQUIRE( vector.size() == 1 );
-	REQUIRE( vector.capacity() == 1 );
-
-	vector.push_back(2);
-	REQUIRE( vector.size() == 2 );
-	REQUIRE( vector.capacity() == 2 );
-
-	vector.push_back(3);
-	REQUIRE( vector.size() == 3 );
-	REQUIRE( vector.capacity() == 4 );
-
-	vector.push_back(4);
-	REQUIRE( vector.size() == 4 );
-	REQUIRE( vector.capacity() == 4 );
-
-	vector.push_back(5);
-	REQUIRE( vector.size() == 5 );
-	REQUIRE( vector.capacity() == 8 );
+    vector_t<double> vector;
+    
+    vector.push_back(1.4);
+    REQUIRE( vector.size() == 1 );
+    REQUIRE( vector.capacity() == 1 );
+    
+    vector.push_back(2);
+    REQUIRE( vector.size() == 2 );
+    REQUIRE( vector.capacity() == 2 );
+    
+    vector.push_back(3);
+    REQUIRE( vector.size() == 3 );
+    REQUIRE( vector.capacity() == 4 );
+    
+    vector.push_back(4);
+    REQUIRE( vector.size() == 4 );
+    REQUIRE( vector.capacity() == 4 );
+    
+    vector.push_back(5);
+    REQUIRE( vector.size() == 5 );
+    REQUIRE( vector.capacity() == 8 );
 }
 
 TEST_CASE("poping elements")
 {
-	vector_t vector;
-
-	vector.push_back(1);
-	vector.push_back(2);
-	vector.push_back(3);
-	vector.push_back(4);
-	vector.push_back(5);
-	vector.push_back(6);
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 5 );
-	REQUIRE( vector.capacity() == 8 );
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 4 );
-	REQUIRE( vector.capacity() == 8 );
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 3 );
-	REQUIRE( vector.capacity() == 8 );
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 2 );
-	REQUIRE( vector.capacity() == 4 );
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 1 );
-	REQUIRE( vector.capacity() == 2 );
-
-	vector.pop_back();
-	REQUIRE( vector.size() == 0 );
-	REQUIRE( vector.capacity() == 1 );
+    vector_t<float> vector;
+    
+    vector.push_back(1);
+    vector.push_back(2);
+    vector.push_back(3);
+    vector.push_back(4);
+    vector.push_back(5);
+    vector.push_back(6);
+    
+    vector.pop_back();
+    REQUIRE( vector.size() == 5 );
+    REQUIRE( vector.capacity() == 8 );
+    
+    vector.pop_back();
+    REQUIRE( vector.size() == 4 );
+    REQUIRE( vector.capacity() == 8 );
+    
+    vector.pop_back();
+    REQUIRE( vector.size() == 3 );
+    REQUIRE( vector.capacity() == 8 );
+    
+    vector.pop_back();
+    REQUIRE( vector.size() == 2 );
+    REQUIRE( vector.capacity() == 4 );
+    
+    vector.pop_back();
+    REQUIRE( vector.size() == 1 );
+    REQUIRE( vector.capacity() == 2 );
+    
+    vector.pop_back();
+    REQUIRE( vector.size() == 0 );
+    REQUIRE( vector.capacity() == 1 );
 }
+
+TEST_CASE("Out of range"){
+    vector_t<float> vector;
+    vector.push_back(1);
+    vector.push_back(3.4);
+    vector.push_back(2.22);
+    REQUIRE_NOTHROW(vector.at(1));
+    REQUIRE_NOTHROW(vector.at(2));
+    REQUIRE_THROWS(vector.at(5));
+}
+
+
